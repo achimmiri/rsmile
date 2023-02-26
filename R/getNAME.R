1dafec3e3fe68690fc4a91b6faf53e79441e9a5a
Ahusha Chimmiri <achimmiri@ipb-halle.de>
2023-02-26 18:44:26 +0100

    adding the function that converts smiles to IUPAC name

---
+++ R/getNAME.R
@@ -0,0 +1,9 @@
+getNAME<-function(getSMI)
+{
+url<- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/SMILES/"
+out<-tryCatch({jsonlite::fromJSON(paste0(url,getSMI, "/JSON"))} ,error = function(x) {return(NA)})
+prop.names  <-tryCatch({out$PC_Compounds$props[[1]][[1]]},error = function(x) {return(NA)})
+prop.values <- tryCatch({out$PC_Compounds$props[[1]][[2]]},error = function(x) {return(NA)})
+IUN<-tryCatch({prop.values[10,"sval"]},error = function(x) {return(NA)})
+return(IUN)
+}
