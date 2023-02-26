7e1465dc040e5b65292d6d6cdba75bc4017dead8
Ahusha Chimmiri <achimmiri@ipb-halle.de>
2023-02-26 18:07:02 +0100

    adding the function that converts smiles to inchikey

---
+++ R/getInchikey.R
@@ -0,0 +1,9 @@
+getInchikey<-function(getSMI)
+{
+url<- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/SMILES/"
+out<-tryCatch({jsonlite::fromJSON(paste0(url,getSMI, "/JSON"))} ,error = function(x) {return(NA)})
+prop.names  <-tryCatch({out$PC_Compounds$props[[1]][[1]]},error = function(x) {return(NA)})
+prop.values <- tryCatch({out$PC_Compounds$props[[1]][[2]]},error = function(x) {return(NA)})
+IK<-tryCatch({prop.values[14 ,"sval"]},error = function(x) {return(0)})
+return(IK)
+}
